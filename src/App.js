3525b7254cdbb535e5e7b4a2b9d65ed61d752231
abhishekrawe <rajputabhisheksingh53@gmail.com>
2022-02-09 00:26:25 +0530

    🎀Work on App.css , added a new css for Product section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './App.css';
 import Header from './Components/Header';
 import Features from './Components/Features';
 import About from './Components/About';
 import Persentation from './Components/Presentaion';
+import MainContent from './Components/mainContent';
 import aboutimage from './images/Frame 19.png'
 import aboutimage1 from './images/download.png';
 import Contact from './Components/Contact';
+import './App.scss'
 
 
 function App() {
@@ -14,6 +17,7 @@ function App() {
       <Header/>
       <Features/>
       <About image={aboutimage} title='Comes with All you need ' button='Get the App'/>
+       <MainContent/>
        <Persentation/>
       <About image={aboutimage1}  title="Download And Get The App Now " button="Download Now"  />
        <Contact/>
---
+++ src/data/product_data.js
@@ -0,0 +1,76 @@
+const product_card = [
+    {
+        id : 1,
+        product_name: "ndfsj",
+        description:"Handakl fgedk opkf fpek",
+        price:350,
+        currency:"$",
+        thumb : "./images/1.png"
+    },
+    {
+        id : 2,
+        product_name: "ndfsj",
+        description:"Handakl fgedk opkf fpek",
+        price:350,
+        currency:"$",
+        thumb : "./images/2.png"
+    },
+    {
+        id : 3,
+        product_name: "ndfsj",
+        description:"Handakl fgedk opkf fpek",
+        price:350,
+        currency:"$",
+        thumb : "./images/3.png"
+    },
+    {
+        id : 4,
+        product_name: "ndfsj",
+        description:"Handakl fgedk opkf fpek",
+        price:350,
+        currency:"$",
+        thumb : "./images/1.png"
+    },
+    {
+        id : 5,
+        product_name: "ndfsj",
+        description:"Handakl fgedk opkf fpek",
+        price:350,
+        currency:"$",
+        thumb : "./images/2.png"
+    },
+    {
+        id : 6,
+        product_name: "ndfsj",
+        description:"Handakl fgedk opkf fpek",
+        price:350,
+        currency:"$",
+        thumb : "./images/3.png"
+    },
+    {
+        id : 7,
+        product_name: "ndfsj",
+        description:"Handakl fgedk opkf fpek",
+        price:350,
+        currency:"$",
+        thumb : "./images/1.png"
+    },
+    {
+        id : 8,
+        product_name: "ndfsj",
+        description:"Handakl fgedk opkf fpek",
+        price:350,
+        currency:"$",
+        thumb : "./images/2.png"
+    },
+    {
+        id : 9,
+        product_name: "ndfsj",
+        description:"Handakl fgedk opkf fpek",
+        price:350,
+        currency:"$",
+        thumb : "./images/3.png"
+    },
+
+]
+export default product_card;
