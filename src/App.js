a9476fb6cf7a21ed0addbdb09d9782d0485d8371
abhishekrawe <rajputabhisheksingh53@gmail.com>
2022-02-08 19:39:36 +0530

    🎃 Created Presetation.jsx this may be cahnges after created a another Presentation section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Header from './Components/Header';
+import Features from './Components/Features';
+import About from './Components/About';
+import Persentation from './Components/Presentaion';
+import aboutimage from './images/Frame 19.png'
+import aboutimage1 from './images/download.png';
+import Contact from './Components/Contact';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Features/>
+      <About image={aboutimage} title='Comes with All you need ' button='Get the App'/>
+       <Persentation/>
+      <About image={aboutimage1}  title="Download And Get The App Now " button="Download Now"  />
+       <Contact/>
     </div>
   );
 }
---
+++ src/Components/Presentaion.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Persentation() {
+  return <div id='presentaion'>
+   <h1> Credit Card </h1>
+   <h1> Lorem ipsum dolor sit amet consectetur.</h1>
+  </div>;
+}
+
+export default Persentation;
